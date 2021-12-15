e55b09d4fac40b21e754da989f94252f06929479
AndreCosta101 <andre.costa101@gmail.com>
2021-12-15 19:34:51 -0300

    Criando repo de especificacao

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,10 +1,21 @@
+import { SpecificationsRepository } from '../repositories/SpecificationsRepository';
 
+interface IRequest {
+  name: string;
+  description: string;
+}
 
 class CreateSpecificationService {
 
+  constructor(private specificationsRepository: SpecificationsRepository) { }
 
-  execute() {
-    console.log("TODO")
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists = this.specificationsRepository.findByName(name);
+    if (specificationAlreadyExists) {
+      throw new Error(`Specification ${name} already exists`);
+    }
+
+    this.specificationsRepository.create({ name, description })
   }
 }
 
